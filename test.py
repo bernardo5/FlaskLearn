a8c1fa5fd65223f79cee5115a24e13fff5329a79
bernardo5 <bernardo.n.gomes@tecnico.ulisboa.pt>
2018-03-10 19:17:34 +0000

    Chapter 3 - templates

--- test.py
+++ test.py
@@ -23,19 +23,34 @@
 # after_request --> register a function to run after each request, if no unhandled exceptions occurred
 # teardown_request --> register a function to run after each request, even if unhandled exceptions ocurred
 
-from flask import Flask, request, make_response, redirect, abort
+#for bootstrap use: (install) pip install flask-bootstrap
+
+#for datetime includes: pip install flask-moment
+
+from flask import Flask, request, make_response, redirect, abort, render_template
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
 
 app=Flask(__name__) #needs to be right after import
+bootstrap=Bootstrap(app)
+moment=Moment(app)
+
+@app.route('/b')
+def example():
+	return render_template('indexb.html')
 
 @app.route('/')
 def index():
-	user_agent=request.headers.get('User-Agent')
-	return '<h1>Hello World</h1> <p>Oh...and your browser is %s</p>' % user_agent
+	'''user_agent=request.headers.get('User-Agent')
+	return '<h1>Hello World</h1> <p>Oh...and your browser is %s</p>' % user_agent'''
+	animals=['lion', 'tiger', 'eagle']
+	return render_template('index.html', animals=animals)
 
 #dynamic display
 @app.route('/user/<name>')
 def user(name):
-	return '<h1>Hello %s!</h1>' % name
+	#return '<h1>Hello %s!</h1>' % name
+	return render_template('user.html', name=name)
 
 #sets a different status code than the 200 one assumed by default
 @app.route('/error/')
@@ -63,6 +78,15 @@ def abortExample():
 	if not user:
 		abort(404)
 	return '<h1>Hello, %s</h1>' % user.name
+
+#error page personalized
+@app.errorhandler(404)
+def page_not_found(e):
+	return '<h1>Blame me</h1> <img src="static/goat.jpg"> ', 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+	return '<h1>Shame</h1>', 500
 
 
 if __name__=='__main__':
